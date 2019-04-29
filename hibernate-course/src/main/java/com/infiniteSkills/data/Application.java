7767c06b6460f9a2a4f8b90bf74ee51be2eff811
RaulCapellanFernandez <raulcape.7@gmail.com>
2019-04-30 00:49:34 +0200

    Ahora si configuracion XML funcional

--- hibernate-course/src/main/java/com/infiniteSkills/data/Application.java
+++ hibernate-course/src/main/java/com/infiniteSkills/data/Application.java
@@ -4,17 +4,28 @@ import java.util.Date;
 
 import org.hibernate.Session;
 
-import com.infiniteSkills.data.entities.*;
+import com.infiniteSkills.data.entities.User;
 
 
 public class Application {
 
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.getTransaction().begin();
 		
-		session.beginTransaction();
+		User user = new User();
+		user.setBirthDate(new Date());
+		user.setCreatedBy("Benito");
+		user.setCreatedDate(new Date());
+		user.setEmailAddress("hola@pene.com");
+		user.setFirstName("Benito");
+		user.setLastName("Camela");
+		user.setLastUpdatedBy("Benito");
+		user.setLastUpdatedDate(new Date());
 		
+		session.save(user);
 		
+		session.getTransaction().commit();
 		
 		session.close();
 	}
